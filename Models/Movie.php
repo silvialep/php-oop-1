1e3ab82187a71a56cf033ce21883f945374cd0fc
silvialep <silvia.lepore_sl@libero.it>
2023-05-03 15:50:10 +0200

    feat: added class + arrays inside object

--- Models/Movie.php
+++ Models/Movie.php
@@ -5,22 +5,24 @@ class Movie {
     public $year;
     public $genres;
     public $director;
-    public $producers;
     public $actors;
     public $isMillennial = false;
 
-    function __construct(string $title, int $year, array $genres, string $director, array $producers, array $actors) {
+    function __construct(string $title, int $year, array $genres, string $director, array $actors) {
         $this->title = $title;
         $this->year = $year;
         $this->genres = $genres;
         $this->director = $director;
-        $this->producers = $producers;
         $this->actors = $actors;
 
 
         if($year < 1981 || $year > 1999) {
             $this->isMillennial = true;
         }
+    }
+
+    public function getStringFromArray() {
+        
     }
 
 
--- index.php
+++ index.php
@@ -1,6 +1,15 @@
 <?php
 
+require_once './Models/Movie.php';
+$movies = [
+    new Movie('Fight Club', '1999', array('Drammatico', 'Thriller', 'Commedia'), 'David Fincher', array('Brad Pitt', 'Edward Norton')),
+];
 
+// var_dump($movies);
+
+// echo $movies[0]->year;
+
+// echo implode(', ', $movies[0]->genres);
 
 ?>
 
@@ -14,14 +23,46 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Movie oop</title>
 
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
 </head>
+
 <body>
 
+    <div class="container pt-3">
+        <h1>Movies</h1>
+        <table class="table">
+            <thead>
+                <tr>
+                    <th>Titolo</th>
+                    <th>Anno</th>
+                    <th>Generi</th>
+                    <th>Regista</th>
+                    <th>Attori principali</th>
+                </tr>
+            </thead>
+            <tbody>
+                <?php
+                for ($i = 0; $i < count($movies); $i++) {
+                ?>
+                    <tr>
+                        <td><?= $movies[$i]->title ?></td>
+                        <td><?= $movies[$i]->year ?></td>
+                        <td><?= implode(', ', $movies[$i]->genres) ?></td>
+                        <td><?= $movies[$i]->director ?></td>
+                        <td><?= implode(', ', $movies[$i]->actors) ?></td>
+
+                    </tr>
+                <?php
+                }
+                ?>
+            </tbody>
+        </table>
+
+    </div>
 
 
 
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js" integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe" crossorigin="anonymous"></script>
 </body>
 
 </html>
